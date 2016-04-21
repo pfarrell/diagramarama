c7c31afbccb6c0c52009577c97f4d72974d13a69
Patrick Farrell <patrick.farrell@grandrounds.com>
2016-04-20 18:48:00 -0700

    gitchy gitchy goo

---
+++ app/models.rb
@@ -0,0 +1,17 @@
+$console = ENV['RACK_ENV'] == 'development' ? Logger.new(STDOUT) : nil
+DB = Sequel.connect(
+  ENV['DIAGRAMARAMA_DB'] || 'postgres://localhost/diagramarama',
+  logger: $console,
+  test: true
+)
+
+DB.sql_log_level = :debug
+DB.extension(:pagination)
+DB.extension(:connection_validator)
+
+DB.pool.connection_validation_timeout = 300
+
+Sequel::Model.plugin :timestamps
+Sequel::Model.plugin :json_serializer
+
+require 'models/my_model'
