a875fe0b34fbcccf797d5114275a6a246f54a1a8
Patrick Farrell <pfarrell@users.noreply.github.com>
2022-03-02 22:17:47 -0500

    update sinatra and change deprecated features (#3)

--- app/routes/root.rb
+++ app/routes/root.rb
@@ -1,9 +1,7 @@
 class Diagramarama < Sinatra::Application
   get "/" do
     diagrams = Diagram.all
-    respond_to do |wants|
-      wants.html { haml :index, locals: { diagrams: diagrams } }
-    end
+      haml :index, locals: { diagrams: diagrams }
   end
 
 end
